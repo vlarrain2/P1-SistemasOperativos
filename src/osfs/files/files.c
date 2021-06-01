933c5762f6d71e2b217298e3208080c5a04e34f3
catalina-ramirez <catalina-ramirez@github.com>
2021-06-01 10:22:48 -0400

    find filename

--- src/osfs/files/files.c
+++ src/osfs/files/files.c
@@ -11,21 +11,46 @@ void os_open(char* filename, char mode){
         if (1){
             osFile* file = malloc(sizeof(osFile));
             file -> name = filename;
-            long int id_absoluto = find_partition(CURRENT_PARTITION);
+            long int id_absoluto = 2048*find_partition(CURRENT_PARTITION);
             long int size_partition = find_partition_size(CURRENT_PARTITION);
+            printf("%li\n", id_absoluto);
             FILE *disk = fopen(DISK_NAME, "rb");
             unsigned char *buffer;
             buffer = malloc(sizeof(char) * BLOCK_SIZE);
             fseek(disk, 1024 + id_absoluto, SEEK_SET);
             fread(buffer, sizeof(char), BLOCK_SIZE, disk);
             printf("hola\n");
+            char entryFileName[29];
+            int result;
+            for (int i = 0; i < 64; i++)
+            {
+                long int aux = buffer[i*32];
+                aux >>= 24;
+
+                if (aux ^ (0x01))
+                {
+                    for (int actual_char = 4; actual_char < 32; actual_char ++)
+                    {
+                        unsigned char byte = buffer[i * 32 + actual_char];
+                        entryFileName[actual_char - 4] = byte;
+                    }
+                    result = strcmp(filename, entryFileName);
+                    if (result == 0)
+                    {
+                        //fclose(file);
+                        //free(buffer);
+                        printf("El archivo %s está en la partición \n", entryFileName);
+                        //return(1);
+                    }
+                }    
+            }
             for (int i = 0; i < 64; i++) //Recorro MBT entrada a entrada
             {
-                long int bytes_directorio = buffer[i*8];
+                long int bytes_directorio = buffer[i*32];
                 for (int j = 1; j < 32; j++)
                 {
                     bytes_directorio <<= 8; // 1111 1111 0000 0000
-                    bytes_directorio += buffer[i*8 + j]; // 1111 1111 1111 1111
+                    bytes_directorio += buffer[i*32 + j]; // 1111 1111 1111 1111
                 }
                 printf("%li\n",bytes_directorio);
                 // if ((CURRENT_PARTITION ^ buffer[i*8]) == 0) // si el and entrega 1111...11
