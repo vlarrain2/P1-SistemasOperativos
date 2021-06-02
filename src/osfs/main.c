96db3ae3d14e5a59aeb7d0a55c5907cabbd6dd11
Alejandro Phillips <aphillips@uc.cl>
2021-06-02 20:31:05 -0300

    main

--- src/osfs/main.c
+++ src/osfs/main.c
@@ -11,10 +11,18 @@ int main(int argc, char **argv)
 {
     os_mount(argv[1], atoi(argv[2]));
     osFile* file = os_open("nene.txt", 'r');
-    unsigned char* buffer = malloc(5);
-    os_read(file, buffer, 2995);
-    os_read(file, buffer, 526);
+    printf("file location: %li\n", file -> location);
+    unsigned char* buffer = malloc(3521);
+    os_read(file, buffer, 3521);
+    printf("BUFFER: %s\n", buffer);
+    //os_read(file, buffer, 526);
+    /*printf("name: %s\n", file -> name);
+    printf("mode: %c\n", file -> mode);
+    char buffer_write[] = "hola";
+    long int write = os_write(file, buffer_write, 5);
+    printf("ESCRIBI %li\n", write);*/
     //os_ls();
     //os_mbt();
     //os_rm("nene.txt");
+    os_close(file);
 }
